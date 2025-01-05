ff556620bc090c6860bebb9bb7e162ce92f5c292
nahidraz23 <nzamanraz@gmail.com>
2025-01-05 23:58:07 +0600

    folder structure upated with contents

---
+++ 01-variable-data-types-methods/02-comments-shortcuts/script.js
@@ -0,0 +1,23 @@
+// This is a single line of code
+
+
+/*
+Multi-line comment
+console.log('Hello World');
+
+console.log(20, 'Hello', true);
+
+const x = 100;
+*/
+
+console.table({name: 'Nahid', email: 'nzamanraz@gmail.com'});
+
+console.group('simple')
+// console.log(x);
+console.error('Alert');
+console.warn('Warning');
+console.groupEnd();
+
+const styles = 'padding: 10px; background-color: white; color: green';
+
+console.log('%cHello World', styles);
