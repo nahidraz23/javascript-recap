353bb37308e70c28fcad9287c61c12a56c39aa3e
nahidraz23 <nzamanraz@gmail.com>
2025-01-08 00:22:53 +0600

    object challenged solved

--- 02-arrays-and-objects/09-object-challenge/script.js
+++ 02-arrays-and-objects/09-object-challenge/script.js
@@ -1,5 +1,6 @@
 let x;
 
+// step 1
 const library = [
     {
         title: "Bela Furabar Age",
@@ -31,5 +32,15 @@ const library = [
 ];
 
 
+// step 2
+library[0].status.read = true;
+library[1].status.read = true;
+library[2].status.read = true;
 
-console.log(library);
+// step 3
+const {title: firstBook} = library[0];
+
+// step 4
+const libraryJSON = JSON.stringify(library);
+
+console.log(libraryJSON);
