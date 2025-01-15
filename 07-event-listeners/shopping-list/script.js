7bc2bb56ec00853e169351c9b06c098eb0b8f75b
nahidraz23 <nzamanraz@gmail.com>
2025-01-15 23:43:26 +0600

    I have learned onclick events

---
+++ 07-event-listeners/shopping-list/script.js
@@ -0,0 +1,28 @@
+const clearBtn = document.querySelector('#clear');
+const ul = document.querySelector('.items');
+
+function onClear() {
+    alert('Clear Items');
+}
+
+// Javascript Event Listener
+// clearBtn.onclick = function () {
+//     alert('Clear Item');
+// };
+
+// clearBtn.onclick = function () {
+//     console.log('Clear Items');
+// }
+
+// addEventListener()
+// clearBtn.addEventListener('click', () => {
+//     alert('Clear Items');
+//     // console.log('Clear Items');
+// });
+
+// clearBtn.addEventListener('click', onClear);
+
+// setTimeout(() => clearBtn.removeEventListener('click', onClear), 5000);
+
+// setTimeout(() => clearBtn.click() , 5000);
+
